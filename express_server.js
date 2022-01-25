d57a781a58ec739574a2d5cb64db378059ec13af
glitjch <t.jamesphan@gmail.com>
2022-01-25 04:22:14 +0000

    adding a function (empty) for next activity, generating a random string

--- express_server.js
+++ express_server.js
@@ -5,12 +5,18 @@ const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -37,3 +43,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/hello", (req, res) => {
 res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+function generateRandomString() {
+
+}
