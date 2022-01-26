557c39b8a20626878ee3e8a2efe9cc59d42b9285
glitjch <t.jamesphan@gmail.com>
2022-01-26 03:55:52 +0000

    added cookies to display user on every page header

--- express_server.js
+++ express_server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 // random id generator
 const generateUid = function() {
@@ -22,6 +24,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// after user logs in their name, use cookie and redirect to home page. display their name
+app.post('/login', (req, res) => {
+  const name = req.body.username;
+  res.cookie("username", name);
+
+  // const requestCookies = req.cookies
+  res.redirect('/urls');
+});
+
 // delete an entry
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -34,7 +45,7 @@ app.post("/urls/:shortURL/update", (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = longURL;
-  console.log(urlDatabase);
+  // console.log(urlDatabase);
   res.redirect('/urls');
 });
 
@@ -51,6 +62,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect(`/urls/${req.params.shortURL}`);
 });
 
+
 // redirecting to specific link outside of tinyapp
 app.get('/u/:shortURL', (req, res) => {
   const short = req.params.shortURL;  
@@ -58,25 +70,32 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 })
 
-
 // redirect to urls because nothing is on root page
 app.get("/", (req, res) => {
   // res.send("Hello!");
   res.redirect('/urls');
 });
 
-
+// main page with the URLs database
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = { 
+      urls: urlDatabase,
+      username: req.cookies.username
+    };
     res.render("urls_index", templateVars);  
 });
 
+// create a new entry URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// After generating a new shortURL, display the long url on a new page with the new short URL generation 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], 
+    username: req.cookies.username
+  };
   res.render("urls_show", templateVars);
 });
 
